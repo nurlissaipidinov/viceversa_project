cbc101d36891e927c75ce966384a0e7b34f1acdd
Nurlis Saipidinov <nurlissaipidinov@MacBook-Pro-Nurlis.local>
2021-03-28 16:50:38 +0600

    First commit

---
+++ viceversa_project/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def about(request):
+    return HttpResponse("This is about page")
+
+
+def home(request):
+    return render(
+        request, 'home.html'
+    )
