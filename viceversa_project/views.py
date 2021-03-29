a21141cf6f23fedcd9ede748585b65e8cd3d1caf
Nurlis Saipidinov <nurlissaipidinov@MacBook-Pro-Nurlis.local>
2021-03-29 10:50:40 +0600

    len of words

--- viceversa_project/views.py
+++ viceversa_project/views.py
@@ -15,7 +15,11 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
+    words = user_text.split()
+    len_of_words = len(words)
     return render(
         request, 'reverse.html', {'usertext': user_text,
-                                  'reversedtext': reversed_text}
+                                  'reversedtext': reversed_text,
+                                  'len_of_words': len_of_words
+                                  }
     )
