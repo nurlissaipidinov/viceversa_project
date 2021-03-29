1a4ca7c8bfac1de6455d3c0ea51544a63648b71f
Nurlis Saipidinov <nurlissaipidinov@MacBook-Pro-Nurlis.local>
2021-03-29 09:59:26 +0600

    Add reverse feature

--- viceversa_project/views.py
+++ viceversa_project/views.py
@@ -10,3 +10,12 @@ def home(request):
     return render(
         request, 'home.html'
     )
+
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    return render(
+        request, 'reverse.html', {'usertext': user_text,
+                                  'reversedtext': reversed_text}
+    )
